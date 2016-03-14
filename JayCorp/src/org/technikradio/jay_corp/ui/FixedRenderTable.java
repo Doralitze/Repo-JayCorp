2d6d6c87f94907813b5cde76616a130012199859
Doralitze <technikradio@icloud.com>
2016-03-14 09:17:59 +0100

    Started to work on the table

--- JayCorp/src/org/technikradio/jay_corp/ui/FixedRenderTable.java
+++ JayCorp/src/org/technikradio/jay_corp/ui/FixedRenderTable.java
@@ -1,15 +1,20 @@
 package org.technikradio.jay_corp.ui;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.LayoutManager;
 
 import javax.swing.JPanel;
 
+import org.technikradio.jay_corp.ui.helpers.MoreColors;
+
 public class FixedRenderTable extends JPanel {
 	private static final long serialVersionUID = 2019095826654681316L;
 
 	private Font f;
+	private FontMetrics lastFM;
 	private String[][] data;
 	private String[] head;
 	private int minWidth = 0;
@@ -48,9 +53,15 @@ public class FixedRenderTable extends JPanel {
 			minHeight = 25;
 		if (head != null) {
 			int a = 15;
-			for (int i = 0; i < head.length; i++) {
-				//Add a with textlenght of head[i]
-			}
+			if (lastFM != null)
+				for (int i = 0; i < head.length; i++) {
+					// Add a with textlenght of head[i]
+					a += lastFM.stringWidth(head[i]);
+				}
+			else
+				for (int i = 0; i < head.length; i++) {
+					a += head[i].length() * 6;
+				}
 		} else
 			minWidth = 50;
 
@@ -58,7 +69,25 @@ public class FixedRenderTable extends JPanel {
 
 	@Override
 	public void paintComponent(Graphics g) {
-		// Draw the header
+		if (f != null)
+			g.setFont(f);
+		lastFM = g.getFontMetrics();
+		// Draw the header (Or use a special component for it)
+		// Draw the body
+		for (int i = 0; i < data.length; i++) {
+			if (i % 2 == 0)
+				g.setColor(Color.LIGHT_GRAY);
+			else
+				g.setColor(MoreColors.VERRY_LIGHT_GRAY);
+			g.fillRect(0, i * 25, this.getWidth(), 25);
+			g.setColor(Color.BLACK);
+			g.drawLine(0, i * 25, this.getWidth(), i * 25);
+			int am = 15;
+			for (int j = 0; j < data[i].length; j++) {
+				g.drawString(data[i][j], am, (i * 25) + 12);
+				am += lastFM.stringWidth(data[i][j]);
+			}
+		}
 
 	}
 
---
+++ JayCorp/src/org/technikradio/jay_corp/ui/helpers/MoreColors.java
@@ -0,0 +1,10 @@
+package org.technikradio.jay_corp.ui.helpers;
+
+import java.awt.Color;
+
+public class MoreColors {
+
+	public static final Color SHIFTED_GREEN = Color.getHSBColor(90, 255, 192);
+	public static final Color VERRY_LIGHT_GRAY = Color.getHSBColor(0f, 0f, 0.9f);
+
+}
