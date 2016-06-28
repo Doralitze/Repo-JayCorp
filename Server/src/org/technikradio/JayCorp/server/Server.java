fad7da626838eed872da4bfce4c9c9793d2ef606
Doralitze <technikradio@icloud.com>
2016-06-28 07:33:34 +0200

    Changed version to current one.

--- Server/src/org/technikradio/JayCorp/server/Server.java
+++ Server/src/org/technikradio/JayCorp/server/Server.java
@@ -33,7 +33,7 @@ import org.technikradio.universal_tools.ParaDate;
 
 public class Server {
 	public static final int port = Integer.parseInt(Settings.getString("Settings.port")); //$NON-NLS-1$
-	public static final String VERSION = "1.3.1.57";
+	public static final String VERSION = "1.3.2.01";
 
 	private static boolean running = true;
 	private static ArrayList<ClientConnector> clients;
