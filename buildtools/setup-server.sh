dde593484491c347146912d48fa3f0704798b2a9
Doralitze <technikradio@icloud.com>
2016-11-08 20:49:48 +0100

    fix3

--- buildtools/setup-server.sh
+++ buildtools/setup-server.sh
@@ -3,7 +3,7 @@
 echo "Installing server to /usr/bin/jc installation directory"
 mkdir /var/jc
 mkdir /var/log/jc
-mkdir /var/bin/jc
+mkdir /usr/bin/jc
 cp -r ./* /usr/bin/jc/
 useradd jcserver -b /usr/bin/jc -d /usr/bin/jc -c "jail user for jaycorp server" -l -M
 chown jcserver /var/jc
