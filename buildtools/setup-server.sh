47a23d78ba1d038730f148d15a51752d8b01b4ae
Doralitze <technikradio@icloud.com>
2016-11-08 20:23:56 +0100

    fixed install script

--- buildtools/setup-server.sh
+++ buildtools/setup-server.sh
@@ -1,16 +1,15 @@
 #! /bin/sh
-scriptname=`basename "$0"`
 
-  if [[ $UID -ne 0 ]]; then
-     echo "${scriptname} must be run as root"
-     exit 1
-  fi
-
-echo "Installing server to current installation directory"
+echo "Installing server to /usr/bin/jc installation directory"
 mkdir /var/jc
 mkdir /var/log/jc
-useradd jcserver -b . -d . -c "jail user for jaycorp server" -l -M 
+mkdir /var/bin/jc
+cp -r ./* /usr/bin/jc/
+useradd jcserver -b /usr/bin/jc -d /usr/bin/jc -c "jail user for jaycorp server" -l -M
 chown jcserver /var/jc
 chown jcserver /var/log/jc
+chown jcserver /usr/bin/jc/*
+chown jcserver /usr/bin/jc/*/*
+chown jcserver /usr/bin/jc/*/*/*
 echo "please edit the following chron file manually in order to set the @reboot flag to the startup script."
-chrontab -e -u jcserver
+crontab -e -u jcserver
